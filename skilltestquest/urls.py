8b72049ff19b2b4ded328ae42f79fbdca9609c5c
jwctwr <jwctwr@gmail.com>
2026-02-22 13:40:51 +0300

    Add home endpoint for root URL

--- skilltestquest/urls.py
+++ skilltestquest/urls.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def home(request):
+    return JsonResponse({
+        'message': 'SkillTestQuest API',
+        'endpoints': {
+            'modules': '/api/modules/',
+            'tasks': '/api/tasks/',
+            'admin': '/admin/',
+            'forum': '/api/comments/',
+            'profile': '/api/user-statistics/',
+            'auth': '/api/token/',
+            'register': '/api/register/'
+        }
+    })
+
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),  
+    path('api/', include('tasks.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
