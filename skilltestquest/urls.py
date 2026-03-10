84324e3cd69d53955c54922f11d72dc906c13f31
jwctwr <jwctwr@gmail.com>
2026-03-10 18:10:45 +0300

    Update urls.py

--- skilltestquest/urls.py
+++ skilltestquest/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView # tyufyth
 
 def home(request):
     return JsonResponse({
@@ -21,6 +21,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # jhbjd
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # yjyd
 ]
